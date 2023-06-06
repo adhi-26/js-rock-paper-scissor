ca0d84293de5574a39fa5e9a184e59c0f2bcca82
Aditya Saran <135217588+adhi-26@users.noreply.github.com>
2023-06-06 17:01:54 +0530

    Create function to determine winner of the round

--- rps.js
+++ rps.js
@@ -21,3 +21,20 @@ function getPlayerChoice(){
     }
 }
 
+function playRound(playerSelection,computerSelection) {
+    let player = 0;
+    let comp = 0;
+    if ((playerSelection=="rock"&&computerSelection=="scissors") ||
+        (playerSelection=="paper"&&computerSelection=="rock") ||
+        (playerSelection=="scissors"&&computerSelection=="paper")) {
+            player = 1;
+            return `You Win! ${playerSelection} beats ${computerSelection}`
+    }else if ((playerSelection=="rock"&&computerSelection=="paper") ||
+    (playerSelection=="paper"&&computerSelection=="scissors") ||
+    (playerSelection=="scissors"&&computerSelection=="rock")) {
+            comp = 1;
+            return `You Lose! ${computerSelection} beats ${playerSelection}`
+    }else {
+        return `Match Drawn!`
+    }
+}
