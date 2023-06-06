f040bcff09203be8e6604deb00da05821c21c7be
Aditya Saran <135217588+adhi-26@users.noreply.github.com>
2023-06-06 16:45:16 +0530

    Create function to get computer choice

--- rps.js
+++ rps.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+function getComputerChoice() {
+    let compChoice  = Math.floor(3 * Math.random());
+    switch (compChoice) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2: 
+            return "scissors";
+    }
+}
